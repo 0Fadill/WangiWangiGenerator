f2767107c13d1bee8857d01f6c74cfdc3370d0d9
Fadill <dafugfadhil@gmail.com>
2021-03-24 03:09:42 +0000

    Initial commit

---
+++ english/main.py
@@ -0,0 +1,12 @@
+name = "ASTOLFO" #Name of the character
+name1 = "ASTOOLFO" #other variations of it
+name3 = "ASTOOOLFOOO"
+name4 = "ASTOOOLFOOOOOOOOOO"
+special = "she has a dick?" #special description of character 
+
+thecursedtexts = '''{0}........... {1} {2} {3} AAAAAAAAA :heart: :heart: :heart: :heart: SO GOOD SOOO GOOOD YOU SMELL SOO GOOD HU HA HU HA HU HA, aaah~ your smell ASTOLFO its so sweet, i like to smell your sweet fragrance {0} AAAAAAAAH ~ ~  Your hair~.... aaah i want to rub your hair betweeen my fingers  ~ ~ ~ ~ ~ AAAAAH {0} YOURE SO SWEET MY GOD :heart: :heart: :heart: EVERYTIME YOU WEAR YOUR STOCKING YOU LOOK SO CUTE AAAAAAAAH {0} YOURE SOO CUTE............ I WILL BURN 12K DOLLARS JUST FOR {0} AAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH
+
+what? {0} is not real? {4} just a fake 2d character you say? No, nonononononononono NOO I DONT TRUST YOU SHE IS REAL NOOO YOU FUCK!! I DONT FUCKING CARE ABOUT REALITY, I DONT FUCKING CARE.
+
+{0} she sees me ... {0} i see her in my laptop she is real, {0} you trust me right?  {0} you wont leave me right? :heart: :heart: :heart: YEAAAAAAAAAAAH I STILL HAVE YOU {0} YOURE ONLY MINE {0} AAAAAAAAAAAAAAH I SEND MY HEART ONLY TO YOU {0} AAAAAAAAH'''
+print(thecursedtexts.format(name, name1, name3, name4, special ))
---
+++ indo/main.py
@@ -0,0 +1,12 @@
+name = "ASTOLFO" #Name of the character
+name1 = "ASTOOLFO" #other variations of it
+name3 = "ASTOOOLFOOO"
+name4 = "ASTOOOLFOOOOOOOOOO"
+special = "dia punya kontol?" #special description of character 
+
+WANGY = '''{0}........... {1} {2} {3} AAAAAAAAA :heart: :heart: :heart: :heart: WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya {0} wangi aku mau nyiumin aroma wanginya {0} AAAAAAAAH ~ ~  Rambutnya.... aaah rambutnya juga pengen aku elus-elus  ~ ~ ~ ~ ~ AAAAAH {0} MANIS BANGET YAAMPUN :heart: :heart: :heart: dia JUGA PAKE STOCKING IMUT BANGET AAAAAAAAH {0} LUCCUUUUUUUUUUUUUUU............ GUA BAKAL BAKAR DUIT 12 JUTA RUPIAH BUAT {0} AAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH
+
+apa ? {0} itu gak nyata ? {4} Cuma karakter 2 dimensi katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI.
+
+{0} ngeliat gw ... {0} di laptop ngeliatin gw {0}... kamu percaya sama aku ? aaaaaaaaaaah syukur {0} gak malu merelakan aku aaaaaah :heart: :heart: :heart: YEAAAAAAAAAAAH GUA MASIH PUNYA {0}, SENDIRI PUN NGGAK MASALAH AAAAAAAAAAAAAAH KIRIMKANLAH CINTAKU PADA {0} KIRIMKAN KE DIA AAAAAAAAH'''
+print(WANGY.format(name, name1, name3, name4, special ))
